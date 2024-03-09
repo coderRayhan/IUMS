12071f28e3622592fc91b48193c698da7b0efd78
coderRayhan <rakibrayhan8@gmail.com>
2024-03-09 11:10:49 +0600

    dashboard card

--- IUMS.Application/Interfaces/Contexts/IDapperContext.cs
+++ IUMS.Application/Interfaces/Contexts/IDapperContext.cs
@@ -6,6 +6,6 @@ namespace IUMS.Application.Interfaces.Contexts
     public interface IDapperContext
     {
         public IDbConnection CreateConnection();
-        Task<bool> IsExist(string tableName, string[] filters, object? param = null);
+        Task<bool> IsExist(string tableName, string[] filters, object param = null);
     }
 }
